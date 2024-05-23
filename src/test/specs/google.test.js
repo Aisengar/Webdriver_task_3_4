f8b312dc741bc7d062d438ddbaee4368eca13d77
Camilo Delgado <camilod@createme.com>
2024-05-23 14:46:44 -0500

    working on task 3 test, adding clickable paths and imputs

--- src/test/specs/google.test.js
+++ src/test/specs/google.test.js
@@ -1,16 +1,49 @@
-const { expect } = require('@wdio/globals')
-const LoginPage = require('../pageobjects/login.page')
-const SecurePage = require('../pageobjects/secure.page')
 
-describe('Task 3 and 4', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open()
+describe('Task 3 and 4 for google cloud', () => {
+    beforeEach(async () => {
+        await browser.url('https://cloud.google.com/?hl=es')
+    })
+    afterEach(async () => {
+        await browser.reloadSession()
+    })
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!')
-        await expect(SecurePage.flashAlert).toMatchElementSnapshot('flashAlert')
+    describe('Task 3 and 4 for google cloud', () => {
+        it('serch for Google Cloud Platform Pricing Calculator ', async () => {
+            //Opening the Google search aplication on the web
+            const googleSerchClick = await $('[class="YSM5S"]');
+            await googleSerchClick.click();
+            // placing the input in the search field and pressing enter
+            const googleSerchInput = await $('[class="mb2a7b"]');
+            await googleSerchInput.setValue('Google Cloud Platform Pricing Calculator');
+            await browser.keys('Enter');
+            
+            //find an element on the list that has the text "Google Cloud Platform Pricing Calculator"
+            const calculatorLink = await browser.$("//a[contains(@href, 'products/calculator') and contains(@class, 'gs-title')]");
+            await calculatorLink.click();
+
+            // click on the "Add to estimate" button
+            const addToEstimate = await $('[class="AeBiU-LgbsSe AeBiU-LgbsSe-OWXEXe-Bz112c-M1Soyc AeBiU-LgbsSe-OWXEXe-dgl2Hf VVEJ3d"]')
+            await addToEstimate.click()
+
+            // click om the "Compute engine" section button
+            const computeEngine = await $('//div[contains(@class, "wrzENe")]//div[contains(@class, "d5NbRd-EScbFb-JIbuQc PtwYlf")]')
+            await computeEngine.click()
+
+            // add the number of instances to 4
+            const waitdisplayedMenu = await $('[class="vHartc"]')
+            await waitdisplayedMenu.waitForDisplayed(1000)
+
+            const numberOfInstances = await $('[id="c11"]')
+            await numberOfInstances.setValue(4)
+            
+
+             //cheking that operating sistem is free
+             const dropdownOpener = await $('//div[contains(@data-field-input-type, "2")]');
+             await dropdownOpener.click();
+             await browser.pause(5000)
+
+        })
     })
 })
+
 
