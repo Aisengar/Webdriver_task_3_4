dbc5746a9b1ead88bceb4a629329440cd31ac4f9
Camilo Delgado <camilod@createme.com>
2024-05-29 18:49:35 -0500

    addin the scrinshot when  an error is trigger

--- src/config/wdio.conf.js
+++ src/config/wdio.conf.js
@@ -226,8 +226,12 @@ exports.config = {
      * @param {boolean} result.passed    true if test has passed, otherwise false
      * @param {object}  result.retries   information about spec related retries, e.g. `{ attempts: 0, limit: 0 }`
      */
-    // afterTest: function(test, context, { error, result, duration, passed, retries }) {
-    // },
+     afterTest: async function(test, context, { error, result, duration, passed, retries }) {
+        if(error){
+            const timestop = new Date().toISOString().replace(/:/g, '-');
+            await browser.saveScreenshot(`./screenshots/${timestop}.png`);
+        }
+     },
 
 
     /**
