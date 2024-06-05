4ba08e3b49533b261b8259a3f6a11f863bad7e13
Camilo Delgado <camilod@createme.com>
2024-06-04 20:29:22 -0500

    adding more verification time and classes to serchproductdisplay

--- src/po/components/serchproductdisplay.component.js
+++ src/po/components/serchproductdisplay.component.js
@@ -1,9 +1,12 @@
 class SerchProductDisplay{
 
     get computeEngine() { return $('(//div[contains(@data-idx,"0")])[1]'); }
+    get serchProductMenu() { return $('[class="wrzENe"]'); }
 
     async clickComputeEngine() {
-        await this.computeEngine.waitForExist({ timeout: 10000 });
+        await this.serchProductMenu.waitForExist({ timeout: 10000 });
+        await browser.pause(500);
+        await this.computeEngine.waitForClickable({ timeout: 10000 });
         await this.computeEngine.click();
     }
 }
